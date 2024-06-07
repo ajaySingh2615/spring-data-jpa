6ff18534e53a33d1672282345624effa5e4d3491
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-06-07 22:31:18 +0530

    "Sorting and pagination"

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +18,37 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+//            generateRandomStudents(studentRepository);
+//            sorting(studentRepository);
+
+//            PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("firstName").ascending());
+//            Page<Student> page = studentRepository.findAll(pageRequest);
+//            System.out.println(page);
+        };
+    }
+
+//    private static void sorting(StudentRepository studentRepository) {
+//        Sort sort = Sort.by( "firstName").ascending().and(Sort.by("age").descending());
+//        studentRepository.findAll(sort)
+//                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+//    }
+
+    private static void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for(int i=0; i<20; i++){
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@gmail.com", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55)
+            );
+            studentRepository.save(student);
+        }
+    }
 }
